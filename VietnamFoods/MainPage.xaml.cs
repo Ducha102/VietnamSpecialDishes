c9e40a82ee07b632bf7c49118287336c914e653c
Ducha102 <ducviet.ict@gmail.com>
2016-06-02 15:50:45 +0700

    VNSpecialFoods_PRJ_UI_50PERC
    Vietnam.Special.Foods Project
    Status: Design UI/UX - 50 percents.

---
+++ VietnamFoods/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using VietnamFoods.Resources;
+
+namespace VietnamFoods
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+      
+
+        private void Go_MainApp_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/View/MainApp.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        
+    }
+}
